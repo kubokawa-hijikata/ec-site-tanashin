5ccc446145b5f66a74e49db8ea984414572f0aa0
soukikubokawa <terra9927sk@gmail.com>
2024-11-20 20:28:31 -0800

    設定したメールアドレスに問い合わせ内容を送信する機能を追加。

--- springboot-app/src/main/java/com/development/springboot_app/controller/ContactController.java
+++ springboot-app/src/main/java/com/development/springboot_app/controller/ContactController.java
@@ -1,12 +1,44 @@
 package com.development.springboot_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.development.springboot_app.services.ContactService;
 
 @Controller
 public class ContactController {
 
     // お問い合わせ内容を送信するための処理を行うクラス
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    // お客様からの問い合わせメールを受信するための処理
+    @PostMapping("/contact")
+    public String getMessage(
+        Model model,
+        @RequestParam(value = "name", required = true) String name,
+        @RequestParam(value = "phoneNumber", required = false) String phoneNumber,
+        @RequestParam(value = "email", required = true) String email,
+        @RequestParam(value = "subject", required = true) String subject,
+        @RequestParam(value = "content", required = true) String content
+    ) {
+        String text = "お名前：" + name + "\n" 
+                    + "電話番号：" + phoneNumber + "\n"
+                    + "メールアドレス：" + email + "\n\n"
+                    + "問い合わせ内容：\n" + content;
+
+        contactService.getMessege(subject, text);
+
+        return "contact";
+    }
 
     // 「contact」画面へ遷移
     @GetMapping("/contact")
---
+++ springboot-app/src/main/java/com/development/springboot_app/services/ContactService.java
@@ -0,0 +1,64 @@
+package com.development.springboot_app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class ContactService {
+
+    private final JavaMailSender eMailSender;
+    
+    @Autowired
+    public ContactService(JavaMailSender eMailSender) {
+        this.eMailSender = eMailSender;
+    }
+
+    // お客様からの問い合わせメールを受信するための処理
+    public void getMessege(String subject, String text) {
+
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                mimeMessage.setRecipient(Message.RecipientType.TO, 
+                new InternetAddress("tanaka.works2024@gmail.com"));
+                mimeMessage.setFrom(new InternetAddress("tanaka.works2024@gmail.com", "お客様からの問い合わせ", "UTF-8"));
+                mimeMessage.setSubject(subject);
+                mimeMessage.setText(text);
+            }
+        };
+
+        try {
+            // eMailSender.send(message);
+            eMailSender.send(preparator);
+        } catch (MailException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // 購入したお客様からの問い合わせメールを受信するための処理
+    public void sendMessege(String toEmail, String text) {
+
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                mimeMessage.setRecipient(Message.RecipientType.TO, 
+                new InternetAddress(toEmail));
+                mimeMessage.setFrom(new InternetAddress("tanaka.works2024@gmail.com", "田中商店", "UTF-8"));
+                mimeMessage.setSubject("ご購入ありがとうございます");
+                mimeMessage.setText(text);
+            }
+        };
+
+        try {
+            // eMailSender.send(message);
+            eMailSender.send(preparator);
+        } catch (MailException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
