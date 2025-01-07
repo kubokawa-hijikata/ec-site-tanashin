4213e935c3a44f1b74a156d153497069c07042e2
soukikubokawa <terra9927sk@gmail.com>
2025-01-07 15:30:54 -0800

    画像がうまく表示されない事象を解決

--- springboot-app/src/main/resources/static/js/function.js
+++ springboot-app/src/main/resources/static/js/function.js
@@ -65,11 +65,11 @@ function popupClose() {
 // 作品画面で他の画像を押下するとメイン画像が切り替わる
 function makeMainLgImage(workId,imageName) {
   let img = document.getElementById("mainLgImage");
-  img.src = `/image/works/${workId}/${imageName}`;
+  img.src = `/works/${workId}/${imageName}`;
 };
 function makeMainMdImage(workId,imageName) {
   let img = document.getElementById("mainMdImage");
-  img.src = `/image/works/${workId}/${imageName}`;
+  img.src = `/works/${workId}/${imageName}`;
 };
 
 // 問い合わせ画面でのバリデーションチェック
